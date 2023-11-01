89a065c5ceecba7b27d829cad195b18beb47ca23
Cristian Lotero Aquino <cristianlotero85@gmail.com>
2023-11-01 01:53:47 -0300

    actualizacion miercoles 01/11/23 madrugada

--- descuentos.js
+++ descuentos.js
@@ -2,19 +2,42 @@
 let valorProducto = document.querySelector("#valorProducto");
 let porcentajeDescuento = document.querySelector("#porcentaje");
 let cupon = document.querySelector("#cupon");
+let sugerencia = "";
+let cuponString;
 const discountButton = document.querySelector("#discountButton");
-const outcome = document.querySelector(".outcome")
+const outcome = document.querySelector(".outcome");
+
 
 discountButton.addEventListener("click", ()=>{
-    if(cupon.value) {
-        let cuponString = cupon.value;
-        console.log(cuponString);
-        console.log(typeof(cuponString));
-        function parseCupon(cuponString){
-            cuponString == "DESCUENTAZO50"? 
-        }
+    const numberValorProducto = Number(valorProducto.value);
+    let outcomeParse ;
+    let numberPorcentaje = 0;
+    if (!numberValorProducto) {
+        outcome.innerHTML = `No se ha ingresado ningun valor`;
+        return
     }
-    
+
+    if(cupon.value) {
+        cuponString = cupon.value;
+
+        (function parseCupon(cupon){
+            cupon === "DESCUENTAZO50" ? numberPorcentaje = 50 
+            : cupon === "DESCUENTAZO40" ? numberPorcentaje = 40 
+            : cupon === "DESCUENTAZO30" ? numberPorcentaje = 30 
+            : cupon === "DESCUENTAZO20" ? numberPorcentaje = 20 
+            : alert(`El cupon no es valido, verifiquelo y vuelva a intentarlo`)
+        })(cuponString);
+    } else {
+        sugerencia = "No se pierda los descuentos haciendose socio!!";
+    }
+    console.log(cuponString === "DESCUENTAZO50");
+    console.log(`El campo de cupon tiene como valor : ${cupon.value}`);
+    console.log(`"cuponString" tiene como valor : ${cuponString}`);
+    console.log(`"numberPorcentaje" tiene como valor : ${numberPorcentaje}`);
+    outcomeParse = (numberValorProducto*(100 - numberPorcentaje)/100);
+    outcome.innerHTML = `El Valor con el descuento aplicado es: $${outcomeParse}  ${sugerencia}`;
+
+
     // const numberValorProducto = Number(valorProducto.value);
     // const numberPorcentaje = Number(porcentajeDescuento.value);
     // if (!numberValorProducto || !numberPorcentaje) {
