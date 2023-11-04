f3ba12a7d03520e3ce3511e7cfb09130220edcc5
Cristian Lotero Aquino <cristianlotero85@gmail.com>
2023-11-03 23:57:26 -0300

    actualizaccion viernes 03/11/23 noche

---
+++ object_coupon.js
@@ -0,0 +1,49 @@
+
+let valorProducto = document.querySelector("#valorProducto");
+let cupon = document.querySelector("#cupon");
+let sugerencia = "";
+let cuponString;
+const discountButton = document.querySelector("#discountButton");
+const outcome = document.querySelector(".outcome");
+
+discountButton.addEventListener("click", () => {
+    const numberValorProducto = Number(valorProducto.value);
+    let outcomeParse;
+    let numberPorcentaje = 0;
+});
+
+function createRandomCoupon (cantidad, palabra) {
+    let totalCouponObj = {};
+        for (cantidad in totalCouponObj) {
+            
+    }
+}
+
+
+
+// discountButton.addEventListener("click", ()=>{
+//     const numberValorProducto = Number(valorProducto.value);
+//     let outcomeParse ;
+//     let numberPorcentaje = 0;
+//     if (!numberValorProducto) {
+//         outcome.innerHTML = `No se ha ingresado ningun valor`;
+//         return
+//     }
+
+//     if(cupon.value) {
+//         cuponString = cupon.value;
+
+//         (function parseCupon(cupon){
+//             cupon === "DESCUENTAZO50" ? numberPorcentaje = 50 
+//             : cupon === "DESCUENTAZO40" ? numberPorcentaje = 40 
+//             : cupon === "DESCUENTAZO30" ? numberPorcentaje = 30 
+//             : cupon === "DESCUENTAZO20" ? numberPorcentaje = 20 
+//             : alert(`El cupon no es valido, verifiquelo y vuelva a intentarlo`)
+//         })(cuponString);
+//     } else {
+//         sugerencia = "No se pierda los descuentos haciendose socio!!";
+//     }
+
+//     outcomeParse = (numberValorProducto*(100 - numberPorcentaje)/100);
+//     outcome.innerHTML = `El Valor con el descuento aplicado es: $${outcomeParse}  ${sugerencia}`;
+// });
