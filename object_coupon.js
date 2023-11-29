b4fc205853b47b9120229906353e89571cc09187
Cristian Lotero Aquino <cristianlotero85@gmail.com>
2023-11-29 01:37:44 -0300

    actualizacion martes 28/11/23 noche

--- object_coupon.js
+++ object_coupon.js
@@ -1,29 +1,70 @@
 
 let valorProducto = document.querySelector("#valorProducto");
+
 let cupon = document.querySelector("#cupon");
 let sugerencia = "";
 let cuponString;
+let found;
 const discountButton = document.querySelector("#discountButton");
 const outcome = document.querySelector(".outcome");
 
-const totalCouponObj = {
-    anioNuevo: 20,
-    carnaval: 20,
-    diaDeLaMujer: 30,
-    malvinas: 30,
-    invierno: 40, 
-    independencia: 50,
-    diaDelNinio: 30,
-    primavera: 40,
-    diaDeLaMadre: 50,
-    finDeAnioDeLocula: 70
-};
+const totalCouponObj = [
+    {
+    cupon: "anioNuevo",
+    descuento: 20,
+    },
+    {
+    cupon: "carnaval",
+    descuento: 20,
+    },
+    {
+    cupon: "diaDeLaMujer",
+    descuento: 30,
+    },
+    {
+    cupon: "malvinas",
+    descuento: 30,
+    },
+    {
+    cupon: "invierno",
+    descuento: 40, 
+    },
+    {
+    cupon: "independencia",
+    descuento: 50,
+    },
+    {
+    cupon: "diaDelNinio",
+    descuento: 30,
+    },
+    {
+    cupon: "primavera",
+    descuento: 40,
+    },
+    {
+    cupon: "diaDeLaMadre",
+    descuento: 50,
+    },
+    {
+    cupon: "finDeAnioDeLocura",
+    descuento: 70,
+    },
+];
+
+
+
 
 discountButton.addEventListener("click", () => {
-    const numberValorProducto = Number(valorProducto.value);
+    let numberValorProducto = Number(valorProducto.value);
     let parseCupon = () => {
-        if (cupon.value == totalCouponObj.)
+        found = totalCouponObj.find(element => element.cupon == cupon.value);
+        console.log(found);
     }
+    parseCupon();
+    
+    let resultado = numberValorProducto * ((100 - found.descuento) / 100);
+
+    outcome.innerText = `El resultado es ${resultado}`;
 });
 
 
