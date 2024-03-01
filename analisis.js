d4e08ceb563df1df9ed6d29f3787c4846a8d6e4e
Cristian Lotero Aquino <cristianlotero85@gmail.com>
2024-03-01 01:02:43 -0300

    actualizado2 Jueves 29/02/24 noche

--- analisis.js
+++ analisis.js
@@ -25,13 +25,25 @@ function medianaPorPersona(nombrePersona) {
 function proyeccionSalarial(nombrePersona) {
     const trabajos = encontrarPersona(nombrePersona).trabajos;
     console.log(trabajos)
-    const salarios = trabajos.map((elemento) => elemento.salario);
+    const salarios = trabajos.map((element) => element.salario);
     console.log(salarios)
     
+    const y = [...salarios];
     const x = [];
     for (let index in salarios) {
-        x.push(index);
+        x.push(+index +1);
     }
 
-    console.log(x)
+    const xy = y.map((element,index) => element * x[index]);
+    const x2 = x.map((element) => element **2);
+    const n = x.reduce((total) => total += 1 );
+    const Ex = x.reduce((total,current)=> total + current);
+    const Ey = y.reduce((total,current)=> total + current);
+    const Exy = xy.reduce((total,current)=> total + current);
+    const Ex2 = x2.reduce((total,current)=> total + current);
+    
+
+    const m = ((Exy - ((Ex * Ey)/ n)) / Ex2 - ((Ex)**2 / n)) 
+
+    const b = (Ey / n) - m * (Ex / n)
 }
