88a76566ab7d459020ae16a9f06b960b2e46a491
Cristian Lotero Aquino <cristianlotero85@gmail.com>
2024-03-03 23:57:44 -0300

    actualizado Domingo 03/03/24 noche

--- analisis.js
+++ analisis.js
@@ -34,6 +34,7 @@ function proyeccionSalarial(nombrePersona) {
         x.push(+index +1);
     }
 
+    
     const xy = y.map((element,index) => element * x[index]);
     const x2 = x.map((element) => element **2);
     const n = x.reduce((total) => total += 1 );
@@ -42,8 +43,50 @@ function proyeccionSalarial(nombrePersona) {
     const Exy = xy.reduce((total,current)=> total + current);
     const Ex2 = x2.reduce((total,current)=> total + current);
     
+    // estas siguientes formulas se suponian que calculaban el siguiente valor de Y pero veo que lo que realmente calcula es "la recta buscada atravez del metodo de los minimos cuadrados" 
+    
+    // const m = ((Exy - ((Ex * Ey)/ n)) / Ex2 - ((Ex)**2 / n)) 
+    // const b = (Ey / n) - m * (Ex / n)
+    // const nextY = (m * x) + b
 
-    const m = ((Exy - ((Ex * Ey)/ n)) / Ex2 - ((Ex)**2 / n)) 
+    const b = (n * Exy - Ex * Ey)/n * Ex2 - (Ex) **2 
+    const a = (Ey - b * Ex) / n
+    const nextY = a + b * (x.length +1) 
 
-    const b = (Ey / n) - m * (Ex / n)
+
+    console.log(nextY)
+}
+
+function testMinimosCuadrados(anios , salarios) {
+    const x = [...anios]
+    const y = [...salarios]
+
+    console.log(x)
+    console.log(y)
+    const xy = y.map((element,index) => element * x[index]);
+    const x2 = x.map((element) => element **2);
+    const n = x.reduce((total) => total += 1 );
+    const Ex = x.reduce((total,current)=> total + current);
+    const Ey = y.reduce((total,current)=> total + current);
+    const Exy = xy.reduce((total,current)=> total + current);
+    const Ex2 = x2.reduce((total,current)=> total + current);
+    
+    // estas siguientes formulas se suponian que calculaban el siguiente valor de Y pero veo que lo que realmente calcula es "la recta buscada atravez del metodo de los minimos cuadrados" 
+    // const m = ((Exy - ((Ex * Ey)/ n)) / Ex2 - ((Ex)**2 / n)) 
+    // const b = (Ey / n) - m * (Ex / n)
+    // const nextY = (m * (x.length + 1)) + b
+    
+    const b = ((n * Exy) - (Ex * Ey)/(n * Ex2) - (Ex**2)) 
+    const a = (Ey - b * Ex) / n
+    const nextY = a + b * (x.length +1) 
+    
+    
+        console.log(`Ex: ${Ex}`)
+        console.log(`Ey: ${Ey}`)
+        console.log(`Exy: ${Exy}`)
+        console.log(`Ex2: ${Ex2}`)
+        console.log(`b: ${b}`)
+        console.log(`a: ${a}`)
+
+    console.log(nextY)
 }
