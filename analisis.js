b9f2b88b672a850168fec4c8e7daa108697f1b7f
Cristian Lotero Aquino <cristianlotero85@gmail.com>
2024-03-04 01:43:26 -0300

    actualizado Domingo 03/03/24 noche

--- analisis.js
+++ analisis.js
@@ -24,16 +24,13 @@ function medianaPorPersona(nombrePersona) {
 
 function proyeccionSalarial(nombrePersona) {
     const trabajos = encontrarPersona(nombrePersona).trabajos;
-    console.log(trabajos)
     const salarios = trabajos.map((element) => element.salario);
-    console.log(salarios)
     
     const y = [...salarios];
     const x = [];
     for (let index in salarios) {
         x.push(+index +1);
     }
-
     
     const xy = y.map((element,index) => element * x[index]);
     const x2 = x.map((element) => element **2);
@@ -43,50 +40,31 @@ function proyeccionSalarial(nombrePersona) {
     const Exy = xy.reduce((total,current)=> total + current);
     const Ex2 = x2.reduce((total,current)=> total + current);
     
-    // estas siguientes formulas se suponian que calculaban el siguiente valor de Y pero veo que lo que realmente calcula es "la recta buscada atravez del metodo de los minimos cuadrados" 
-    
-    // const m = ((Exy - ((Ex * Ey)/ n)) / Ex2 - ((Ex)**2 / n)) 
-    // const b = (Ey / n) - m * (Ex / n)
-    // const nextY = (m * x) + b
-
-    const b = (n * Exy - Ex * Ey)/n * Ex2 - (Ex) **2 
+    const b = ((n * Exy) - (Ex * Ey))/((n * Ex2) - (Ex**2)) 
     const a = (Ey - b * Ex) / n
-    const nextY = a + b * (x.length +1) 
-
+    let nextY = (a + b * (x.length +1)).toFixed(2) 
 
     console.log(nextY)
 }
 
-function testMinimosCuadrados(anios , salarios) {
-    const x = [...anios]
-    const y = [...salarios]
+function proyeccionPorPersona(nombrePersona) {
+    const trabajos = encontrarPersona(nombrePersona).trabajos;
 
-    console.log(x)
-    console.log(y)
-    const xy = y.map((element,index) => element * x[index]);
-    const x2 = x.map((element) => element **2);
-    const n = x.reduce((total) => total += 1 );
-    const Ex = x.reduce((total,current)=> total + current);
-    const Ey = y.reduce((total,current)=> total + current);
-    const Exy = xy.reduce((total,current)=> total + current);
-    const Ex2 = x2.reduce((total,current)=> total + current);
-    
-    // estas siguientes formulas se suponian que calculaban el siguiente valor de Y pero veo que lo que realmente calcula es "la recta buscada atravez del metodo de los minimos cuadrados" 
-    // const m = ((Exy - ((Ex * Ey)/ n)) / Ex2 - ((Ex)**2 / n)) 
-    // const b = (Ey / n) - m * (Ex / n)
-    // const nextY = (m * (x.length + 1)) + b
-    
-    const b = ((n * Exy) - (Ex * Ey)/(n * Ex2) - (Ex**2)) 
-    const a = (Ey - b * Ex) / n
-    const nextY = a + b * (x.length +1) 
-    
-    
-        console.log(`Ex: ${Ex}`)
-        console.log(`Ey: ${Ey}`)
-        console.log(`Exy: ${Exy}`)
-        console.log(`Ex2: ${Ex2}`)
-        console.log(`b: ${b}`)
-        console.log(`a: ${a}`)
+    let porcentajesCrecimiento = []
 
-    console.log(nextY)
+    for (let i= 1; i < trabajos.length ; i++ ) {
+        const salarioActual = trabajos[i].salario;
+        const salarioPasado = trabajos[i -1].salario;
+        const incremento = salarioActual - salarioPasado;
+        const porcentajeCrecimiento = incremento / salarioPasado;
+        porcentajesCrecimiento.push(porcentajeCrecimiento)
+    }
+    console.log(porcentajesCrecimiento)
+
+    const medianaPorcentajesCrecimiento = PlatziMath.calcularMediana(porcentajesCrecimiento);
+    // console.log(porcentajesCrecimiento, medianaPorcentajesCrecimiento)
+    // console.log(salariosPersona)  
+    const nuevoSalarioPersona = trabajos[trabajos.length -1].salario * (1 + medianaPorcentajesCrecimiento)
+    console.log(nuevoSalarioPersona.toFixed(2))
 }
+
